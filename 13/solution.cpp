330c3a5272d0ba0f5b9ced18961c812b29ae810d
mantlemoose <alexdu9876@gmail.com>
2020-11-08 17:53:58 -0800

    C++ solution

---
+++ 13/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <utility>
+#include <map>
+#include <list>
+using namespace std;
+typedef pair <int, int> pii;
+typedef long long ll;
+#define f first
+#define s second
+
+int n;
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+
+}
+
