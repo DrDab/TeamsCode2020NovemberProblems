32a9bc11c35195844bf81a3b71e099922447b8fc
cyin100 <46613498+cyin100@users.noreply.github.com>
2020-11-13 20:40:49 -0800

    Update solution.java
    java 3 soln

--- 3/solution.java
+++ 3/solution.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class solution {
-  public static void main(String[]args) {
+	public static void main(String[]args) {
 		Scanner s = new Scanner(System.in);
 		char c = s.next().charAt(0);
 		String str = s.nextLine();
