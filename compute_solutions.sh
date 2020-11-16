3e4d61904508cebfb85c4a55c1f0ddf92a4018ba
mantlemoose <alexdu9876@gmail.com>
2020-11-15 21:47:39 -0800

    Reorganized

--- compute_solutions.sh
+++ compute_solutions.sh
@@ -1,2 +1,2 @@
-for f in $(ls io/*.in); do echo computing $f; ./a.out < $f > $(echo $f | sed 's/in$/out/'); done
+for f in $(ls cases/*.in); do echo computing $f; ./a.out < $f > $(echo $f | sed 's/in$/out/'); done
 
