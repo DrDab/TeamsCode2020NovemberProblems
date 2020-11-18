507f3536a390bfec0b164f3a26180c49d6a1abfe
Exr0n <mail@exr0n.com>
2020-11-17 23:55:27 -0800

    NW: #19 this has an error (with test.in, solution and brute)
    but changing line 56 for auto b=a doesn't.?

--- 19/solution.cpp
+++ 19/solution.cpp
@@ -7,17 +7,19 @@
 
 using namespace std;
 const int MX = 1e6;
-int N, R, x[MX], y[MX];
+int N, R, ax[MX], y[MX];
 
 double dist(int i, int j)
 {
-    return sqrt((double)(x[i]-x[j])*(x[i]-x[j]) + (double)(y[i]-y[j])*(y[i]-y[j]));
+    return sqrt((double)(ax[i]-ax[j])*(ax[i]-ax[j]) + (double)(y[i]-y[j])*(y[i]-y[j]));
 }
 double area(int i, int j, int k)
 {
     double a = dist(i, j);
     double b = dist(j, k);
     double c = dist(k, i);
+    if (a > R || b > R || c > R) return 0;
+    // herons formula
     double s = (double)(a+b+c)/2;
     return sqrt(s * (s-a) * (s-b) * (s-c));
 }
@@ -26,32 +28,38 @@ int main()
 {
     scanf("%d%d", &N, &R);
     for (int i=0; i<N; ++i)
-        scanf("%d%d", &x[i], &y[i]);
-    for (int i=0; i<N; ++i) printf("%d: %d %d\n", i, x[i], y[i]);
-    printf("%lf\n", area(0, 1, 2));
+        scanf("%d%d", &ax[i], &y[i]);
     priority_queue<pair<int, int>, deque<pair<int, int> >, greater<pair<int, int> > > pq; // state = < x, idx >
 
     set<pair<int, int> > active;    // state = < y, idx >
     queue<pair<int, set<pair<int, int> >::iterator > > rem; // state = < x, remove_iterator >
 
-    for (int i=0; i<N; ++i) pq.push(make_pair(x[i], i));
+    double ans = 0;
+
+    for (int i=0; i<N; ++i) pq.push(make_pair(ax[i], i));
     while (!pq.empty())
     {
         pair<int, int> cur = pq.top(); pq.pop();
         int x = cur.first, i = cur.second;
-        printf("%d %d\n", x, i);
-        // make sure second dimension doesn't exclude something
+        //printf("%d: %d %d\n", i, x, y[i]);
         while (rem.size() && x - rem.front().first > R)
         {
-            printf("popping at %d\n", rem.front().first);
+            //printf("popping at %d\n", rem.front().first);
             active.erase(rem.front().second);
             rem.pop();
         }
-        auto lower = lower_bound(active.begin(), active.end(), make_pair(y[i], -1));
-        auto upper = upper_bound(active.begin(), active.end(), make_pair(y[i], N+1));
-        for (auto it=lower; it!=upper; ++it)
-            printf("    %d (%d, %d)\n", it->second, it->first, y[it->second]);
+        //printf("set:  "); for (auto p : active) printf("%3d (%-2d %2d)  ", p.second, ax[p.second], p.first); printf("\n");
+        // make sure second dimension doesn't exclude something
+        auto lower = lower_bound(active.begin(), active.end(), make_pair(y[i]-R, -1));
+        auto upper = upper_bound(active.begin(), active.end(), make_pair(y[i]+R, N+1));
+        for (auto a=lower; a!=upper; ++a)
+            for (auto b=lower; b!=upper; ++b)
+            {
+                printf("points %d, %d, %d => %lf\n", a->second, b->second, i, area(a->second, b->second, i));
+                if (a->second != b->second) ans = max(ans, area(a->second, b->second, i));
+            }
         set<pair<int, int> >::iterator g = active.insert(make_pair(y[i], i)).first;
         rem.push(make_pair(x, g));
     }
+    printf("%lld\n", (long long)(ans*1000));
 }
