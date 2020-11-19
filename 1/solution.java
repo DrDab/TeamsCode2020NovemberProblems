412fdcc84293a1af0df2ba01f9836be8cc105fd2
cyin100 <46613498+cyin100@users.noreply.github.com>
2020-11-18 20:56:07 -0800

    soln 1 javA
    MIGHT NEED TO CHANGE LATER BC OF ESCAPE CHARACTERS

---
+++ 1/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class solution {
+	
+	public static void main(String[]args) {
+		
+		System.out.println(" /\\   /\\ ");
+		System.out.println("//\\\\_//\\\\");
+		System.out.println("\\_     _/");
+		System.out.println(" / * * \\ ");
+		System.out.println(" \\_\\O/_/ ");
+
+	}
+}
