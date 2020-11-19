d99c3af4629d51c826f99b8e688bb6f049a48003
mantlemoose <alexdu9876@gmail.com>
2020-11-18 21:56:09 -0800

    C++ solution

--- 16/solution.cpp
+++ 16/solution.cpp
@@ -14,10 +14,13 @@ typedef long long ll;
 #define f first
 #define s second
 
-int n;
-double med;
+const int maxn = 1e6 + 10;
+
+int n, med;
 priority_queue <int, vector <int>, less <int> > lo;
 priority_queue <int, vector <int>, greater <int> > hi;
+int cnt, mode;
+map <int, int> mp;
 
 void med_ins(int x) {
     if (med < x) {
@@ -25,12 +28,12 @@ void med_ins(int x) {
             lo.push(hi.top());
             hi.pop();
             hi.push(x);
-            med = ((double)lo.top() + hi.top()) / 2;
+            med = (lo.top() + hi.top()) / 2;
         }
         else {
             hi.push(x);
             if (hi.size() > lo.size()) med = hi.top();
-            else med = ((double)lo.top() + hi.top()) / 2;
+            else med = (lo.top() + hi.top()) / 2;
         }
     }
     else {
@@ -38,35 +41,33 @@ void med_ins(int x) {
             hi.push(lo.top());
             lo.pop();
             lo.push(x);
-            med = ((double)lo.top() + hi.top()) / 2;
+            med = (lo.top() + hi.top()) / 2;
         }
         else {
             lo.push(x);
             if (lo.size() > hi.size()) med = lo.top();
-            else med = ((double)lo.top() + hi.top()) / 2;
+            else med = (lo.top() + hi.top()) / 2;
         }
     }
 }
 
-int get_mode()
-{
-    return 0;
-}
-
 int main() {
     scanf("%d", &n);
-    ll prev=0, mn=0, mx=0, sum=0;
+    int mn=0, mx=0;
+    ll sum=0;
     for (int i = 0; i < n; ++i) {
         int x; scanf("%d", &x);
         med_ins(x);
+        mp[x]++;
+        if (mp[x] >= cnt) {
+            mode = x;
+            cnt = mp[x];
+        }
         if (!i) mn = mx = x;
+        mn = min(mn, x);
+        mx = max(mx, x);
         sum += x;
-        printf("%lld %lld %.0lf %.0lf %d", mn, mx, (double)sum / (i+1), med, get_mode());
-        /*if (op == 3) //mean
-        if (op == 4) //min
-        if (op == 5) //max
-        if (op == 6) //standard deviation
-        if (op == 7) //mode*/
+        printf("%d %d %.0lf %d %d\n", mn, mx, (double)sum / (i+1), med, mode);
     }
 }
 
