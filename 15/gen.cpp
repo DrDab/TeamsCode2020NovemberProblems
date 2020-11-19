a1488a938b83cf122dc8e320493948f884190feb
Exr0n <mail@exr0n.com>
2020-11-19 12:05:05 -0800

    cases + io for 15

--- 15/gen.cpp
+++ 15/gen.cpp
@@ -1,37 +1,18 @@
-#include<bits/stdc++.h>
-#define uset unordered_set
-#define umap unordered_map
-using namespace std;
-umap<int,set<int>> f;
-int rand(const int min, const int max)
-{
-    return min + rand() % (max - min + 1);
-}
+#include <ctime>
+#include <cstdio>
+#include <cstdlib>
+#define rand(l,h) (rand()%(h-l+1)+l)
+
+int N = 100000;
+int M = 10000000;
+
 int main()
 {
     srand(time(0));
-    const int n=rand(99990,100000);
-    const int m=rand(4999990,10000000);
-    cout<<n<<" "<<m<<endl;
-    int s=rand(1,n);
-    for (int i = 1; i <= n; i++)
-    {
-        if(i==s){
-            continue;
-        }
-        int u=rand(1,n);
-        while(u==i||f[i].find(u)!=f[i].end()){
-            u=rand(1,n);
-        }
-        f[u].insert(i);
-        cout<<u<<" "<<i<<endl;
-    }
-    
-    for (int i = 0; i < m; i++)
-    {
-        const int a=rand(1,n);
-        const int b=rand(1,n);
-        cout<<a<<" "<<b<<endl;
-    }
+    printf("%d %d\n", N, M);
+    for (int i=2; i<=N; ++i) printf("%d %d\n", rand(1, i-1), i);
+    for (int i=0; i<M; ++i) printf("%d %d\n", rand(1, N), rand(1, N));
+
     return 0;
 }
+
