ba671406a841b8831220cbf9936f3baf09d96921
Exr0n <mail@exr0n.com>
2020-11-20 00:44:36 -0800

    some solution debugging
    TODOS:
    - figure out whether #15 solution is correct or not
    - fix python solution and see if it runs in time
    - re-export #15
    - export 5-20 for advanced division

--- 1/solution.cpp
+++ 1/solution.cpp
@@ -1,17 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <string.h>
-#include <utility>
-#include <map>
-#include <list>
-using namespace std;
-typedef pair <int, int> pii;
-typedef long long ll;
-#define f first
-#define s second
+#include <cstdio>
 
 int main() {
     printf("    ______    ____\n   (      )  |  o |\n  |        |/ ___U|\n@_|_________/\n  |_|_| |_|_|\n");
