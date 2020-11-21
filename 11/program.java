d537596ee7eac87073a4cddc2810eab3b13ee2f9
Doki_Fusky <duvictor514@gmail.com>
2020-11-21 12:35:18 -0800

    Update program.java

--- 11/program.java
+++ 11/program.java
@@ -1,14 +1,15 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class program
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        Scanner input = new Scanner(System.in);
-        String[] l1 = input.nextLine().split(" ");
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String[] l1 = input.readLine().split(" ");
         int N = Integer.parseInt(l1[0]);
         int M = Integer.parseInt(l1[1]);
-        String[] l2 = input.nextLine().split(" ");
+        String[] l2 = input.readLine().split(" ");
         int[] pSum = new int[N];
         int[] answers = new int[M];
         for (int i = 0; i < N; i++)
@@ -18,7 +19,7 @@ public class program
         }
         for (int i = 0; i < M; i++)
         {
-            answers[i] = pSum[Integer.parseInt(input.nextLine()) - 1];
+            answers[i] = pSum[Integer.parseInt(input.readLine()) - 1];
         }
         for (int i = 0; i < M; i++)
         {
