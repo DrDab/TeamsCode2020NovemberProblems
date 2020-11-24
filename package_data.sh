7494f34089364f88531a437dfdffee308dc7b37a
Harry Chen <harrychen0314@gmail.com>
2020-11-23 17:52:03 -0800

    update packaging script to achieve same behavior on linux and macos

--- package_data.sh
+++ package_data.sh
@@ -7,7 +7,8 @@ zip "../../$1/$2.zip" *
 cd $cur
 
 mkdir tmp 2>/dev/null
-echo "-p $3 -t 4 -m 262144\n" > config # 256mb
+echo "-p $3 -t 4 -m 262144" > config # 256mb
+echo >> config
 for i in {1..5}; do
     echo "test $i.in $i.out" >> config
 done
